1204962751dbbaf5d3a9963aab2d7836e74fa180
bagwuel <mbagwu39@gmail.com>
2023-04-02 18:30:09 +0100

    complete linked list data structure

---
+++ README.md
@@ -0,0 +1,35 @@
+# odin-project-linked-list
+# linked list functionalities
+
+# append(data) - adds a new node containing value to the end of the list
+# prepend(data) - adds a new node containing value to the start of the list
+# getSize() - returns the total number of nodes in the list
+# getHead() - returns the first node in the list
+# getTail() - returns the last node in the list
+# at(index) - returns the node at the given index
+# shift() - removes the first element from the list
+# pop() - removes the last element from the list
+# contains(data) - returns true if the passed in value is in the list and otherwise returns false.
+# find(data) - returns the index of the node containing value, or null if not found.
+# toString() - represents the LinkedList objects as strings, so you can print them out and preview them in the console. The format is: ( value ) -> ( value ) -> ( value ) -> null
+# insertAt(data, index) - inserts a new node with the provided value at the given index.
+# removeAt(index) - removes the node at the given index.
+# isEmpty - returns true if the list is empty and false otherwise
+
+
+# time complexity analysis
+
+# getHead - O(1),
+# getTail - O(1),
+# getSize - O(1),
+# prepend - O(1),
+# append - O(1),
+# at - O(n),
+# shift - O(1),
+# pop - O(n),
+# contains - O(n),
+# find - O(n),
+# toString - O(n),
+# insertAt - O(n),
+# removeAt - O(n),
+# isEmpty - O(1),
---
+++ linked_list.js
@@ -0,0 +1,176 @@
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    }
+    getHead = () => this.head
+    getTail = () => this.tail
+    getSize = () => this.size
+    prepend(data) {
+        const newNode = new Node(data);
+        if (this.isEmpty()) {
+            this.head = newNode
+            this.tail = newNode
+        }else {
+            newNode.next = this.head
+            this.head = newNode
+        }
+        this.size++
+    }
+    append(data) {
+        const newNode = new Node(data)
+        if (this.isEmpty()) {
+            this.head = newNode
+            this.tail = newNode
+        }else {
+            this.tail.next = newNode
+            this.tail = newNode
+        }
+        this.size++
+    }
+    at(index) {
+        let curr = null
+        let i = 0
+        if (!this.isEmpty() && (index < this.size && index >= 0)) {
+            curr = this.head
+            while (i < index) {
+                curr = curr.next
+                i++;
+            }
+        }
+        return (curr)
+    }
+    shift() {
+        if (this.isEmpty())
+            return (null)
+        let head = this.head
+        this.head = this.head.next
+        if (this.tail === head)
+            this.tail = null
+        this.size--
+        return (head)
+    }
+    pop() {
+        let curr = this.head
+        if (this.isEmpty())
+            return (null)
+        if (this.head === this.tail) {
+            this.head = this.tail = null
+        }else {
+            while (curr.next !== this.tail)
+                curr = curr.next
+            this.tail = curr
+            curr = curr.next
+            this.tail.next = null
+        }
+        this.size--
+        return (curr)
+    }
+    contains(data) {
+        let curr = this.head
+        if (this.isEmpty())
+            return (false)
+        while (curr !== null) {
+            if (data === curr.data)
+                return (true)
+            curr = curr.next
+        }
+        return (false)
+    }
+    find(data) {
+        let curr = this.head
+        if (this.isEmpty())
+            return (null)
+        while (curr !== null) {
+            if (data === curr.data)
+                return (curr)
+            curr = curr.next
+        }
+        return (null)
+    }
+    toString() {
+        let curr = this.head
+        let list = ""
+        while (curr !== null) {
+            list += `( ${curr.data} ) -> `;
+            curr = curr.next
+        }
+        list += 'null'
+        console.log(list);
+    }
+    insertAt(data, index) {
+        if (index < 0 || index >= this.size)
+            return;
+        if (index === 0){
+            this.prepend(data)
+            return;
+        }else {
+            const newNode = new Node(data)
+            let i = 0;
+            let curr = this.head
+            while (i < index - 1) {
+                curr = curr.next
+                i++;
+            }
+            newNode.next = curr.next
+            curr.next = newNode
+            this.size++
+        }
+    }
+    removeAt(index) {
+        let curr = null
+        let prev = null
+        let i = 0
+        if (index === 0)
+            return this.shift()
+        if (!this.isEmpty() && (index < this.size && index >= 0)) {
+            curr = this.head
+            while (i < index) {
+                prev = curr
+                curr = curr.next
+                i++;
+            }
+            prev.next = curr.next
+            this.size--
+        }
+        return (curr)
+    }
+    isEmpty = () => this.head === null && this.tail === null;
+}
+
+const myList = new LinkedList();
+myList.append(3)
+myList.append(4)
+myList.append(5)
+myList.append(6)
+myList.prepend('hi')
+myList.prepend('me')
+myList.prepend('you')
+console.log(myList.at('1'));
+console.log(myList.pop());
+console.log(myList.contains(4));
+console.log(myList.find(4));
+console.log(myList.find('4'));
+myList.toString()
+console.log(myList.shift());
+console.log(myList.shift());
+myList.toString()
+console.log(myList.getSize());
+console.log(myList.removeAt(0));
+myList.toString()
+myList.insertAt(12, 2)
+myList.toString()
+myList.insertAt(12, 0)
+myList.toString()
+myList.insertAt(12, 18)
+myList.toString()
+myList.insertAt(22, 5)
+myList.toString()
